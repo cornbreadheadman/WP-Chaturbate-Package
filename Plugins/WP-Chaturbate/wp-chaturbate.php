3a9fb01e5f2523686ec3eebce376cd6758e2e110
Jason Dobson <jasonldobson@gmail.com>
2019-01-21 11:24:20 -0500

    Create wp-chaturbate.php

---
+++ Plugins/WP-Chaturbate/wp-chaturbate.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP Chaturbate
+Plugin URI: https://github.com/cornbreadheadman/WP-Chaturbate-Package/Plugins/WP-Chaturbate
+Description: Plugin to show chaturbate.com webcams on your Wordpress page
+Version: 0.0.1
+Author: CamUnderGround.Com
+Author URI: http://camunderground.com/
+License: GPL3
+Text Domain: wp-chaturbate
+*/
+?>
